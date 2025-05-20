3e87312ff6961b4d113575328efca9e45e45a06a
WILSONBARKERO\barke <barkero.wilson@gmail.com>
2025-05-20 21:54:19 +0800

    created starting routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use function PHPUnit\Framework\returnArgument;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+Route::get('/xxxx', function(){
+    return 'my nibba';
+})->name('hello');
+
+Route::get('/hallo', function(){
+    return redirect()->route('hello');
+});
+
+route::get('/greet/{name}', function($name){
+    return 'hello ' . $name . ' ?';
+});
+
+route::fallback(function (){
+    return 'THIS IS 404 PAGE MF!';
 });
