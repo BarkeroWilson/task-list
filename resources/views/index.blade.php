6089ad81b63d7512995d4f2ee3edd8a86b3d450b
WILSONBARKERO\barke <barkero.wilson@gmail.com>
2025-05-20 23:34:21 +0800

    added blade templates and docker

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title')
+<h1>
+    OUR TASKS
+</h1>
+@endsection
+
+@section('content')
+<div>
+
+        @forelse ($tasks as $task)
+            <div>
+                <a href="{{route('tasks.show', ['id' => $task->id]) }}">{{$task-> title}}</a>
+            </div>
+            @empty
+                <div>NO MORE TASKS</div>
+        @endforelse
+
+</div>
+@endsection
+
